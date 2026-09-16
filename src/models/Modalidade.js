a96484505aa35d5bcfb9cd4d80a217d909311535
Rodolfo Oliveira <47088607+dorfusouza@users.noreply.github.com>
2026-09-16 09:20:02 -0300

    separação em módulos

---
+++ src/models/Modalidade.js
@@ -0,0 +1,11 @@
+// 1. Enumeração (herdada da Aula 03, sem mudanças)
+const Modalidade = Object.freeze({
+    FUTSAL_MASCULINO: "Futsal Masculino",
+    FUTSAL_FEMININO: "Futsal Feminino",
+    VOLEI_MISTO: "Vôlei Misto",
+    BASQUETE_MASCULINO: "Basquete Masculino",
+    BASQUETE_FEMININO: "Basquete Feminino",
+    HANDEBOL_MISTO: "Handebol Misto",
+});
+
+module.exports = Modalidade;
